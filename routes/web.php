3cccd65fb697907a8f355a7e9a9bc1a9e021c567
amabdelftah <ahmedabdelftah95165@gmail.com>
2023-01-31 21:23:48 +0200

    Add initial ui

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,25 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function () {
-    $books = ['ahmed', 'mohamed'];
+
+    $posts = [
+        [
+            'id' => 1,
+            'title' => 'laravel',
+            'description' => 'test',
+            'posted_by' => 'Ahmed',
+            'created_at' => '2022-10-01 01:00:00',
+        ],
+        [
+            'id' => 2,
+            'title' => 'java script',
+            'description' => 'test 123',
+            'posted_by' => 'Mohamed',
+            'created_at' => '2022-09-10 02:00:00',
+        ],
+    ];
 
     return view('test', [
-        'books' => $books,
-        'name' => 'ahmed',
+        'posts' => $posts,
     ]);
 });
