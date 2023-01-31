d1b258db2be608df35310479cb4c1874da19b0d8
amabdelftah <ahmedabdelftah95165@gmail.com>
2023-01-31 22:17:15 +0200

    Add show post and links

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -29,4 +29,18 @@ class PostController extends Controller
             'posts' => $posts,
         ]);
     }
+
+    public function show($postId)
+    {
+//        dd($postId);
+        $post = [
+            'id' => 1,
+            'title' => 'laravel',
+            'description' => 'test',
+            'posted_by' => 'Ahmed',
+            'created_at' => '2022-10-01 01:00:00',
+        ];
+
+        return view('posts.show', ['post' => $post]);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -16,41 +16,14 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link active" href="#">All Posts</a>
+                <a class="nav-link active" href="/posts">All Posts</a>
             </div>
         </div>
     </div>
 </nav>
 <div class="container">
-    <div class="text-center">
-        <button type="button" class="mt-4 btn btn-success">Create Post</button>
-    </div>
-    <table class="table mt-4">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Title</th>
-            <th scope="col">Posted By</th>
-            <th scope="col">Created At</th>
-            <th scope="col">Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($posts as $post)
-        <tr>
-            <td>{{$post['id']}}</td>
-            <td>{{$post['title']}}</td>
-            <td>{{$post['posted_by']}}</td>
-            <td>{{$post['created_at']}}</td>
-            <td>
-                <a href="/posts/{{$post['id']}}" class="btn btn-info">View</a>
-                <a href="#" class="btn btn-primary">Edit</a>
-                <a href="#" class="btn btn-danger">Delete</a>
-            </td>
-        </tr>
-        @endforeach
-        </tbody>
-    </table>
+    we are in show {{$post['title']}}
+
 
 </div>
 
