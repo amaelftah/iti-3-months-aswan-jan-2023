a3f82f36aabc5c3b894a162c4b3afe1273af1224
amabdelftah <ahmedabdelftah95165@gmail.com>
2023-01-31 21:54:25 +0200

    Views naming

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -25,7 +25,7 @@ class PostController extends Controller
             ],
         ];
 
-        return view('test', [
+        return view('posts.index', [
             'posts' => $posts,
         ]);
     }
