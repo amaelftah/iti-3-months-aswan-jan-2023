ae9bfbb53d735fceef8e024a4962c5c547d486b1
amabdelftah <ahmedabdelftah95165@gmail.com>
2023-02-01 20:53:25 +0200

    Add layout/app.blade.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Route::get('/posts', [PostController::class, 'index']);
 
-Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 
