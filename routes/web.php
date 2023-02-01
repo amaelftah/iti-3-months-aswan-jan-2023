b68e30bbae4c6b67e8edb751aaa04de3215ca3e7
amabdelftah <ahmedabdelftah95165@gmail.com>
2023-02-01 20:36:45 +0200

    Add store action

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,8 @@ Route::get('/', function () {
 
 Route::get('/posts', [PostController::class, 'index']);
 
+Route::get('/posts/create', [PostController::class, 'create']);
+
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
